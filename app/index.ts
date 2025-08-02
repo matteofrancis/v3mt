6fad6ba715f44cc14025753ec21e8179c4aea19d
Matthew Friemoth <35771739+matteofrancis@users.noreply.github.com>
2025-08-02 17:40:12 -0500

    set versions

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-
 import init from './cli/init/init.js';
 import launchGame from './cli/launch-game.js';
 import openErrorLog from './cli/open-error-log.js';
@@ -10,10 +9,14 @@ import openModFolder from './cli/open-mod-folder.js';
 import sendToGame from './cli/send-to-game.js';
 import setupVSCTasks from './cli/setup-vsc-tasks/setup-vsc-tasks.js';
 import setupModFolders from './cli/setup-mod-folders.js';
+import packageJson from '../package.json' with { type: 'json' };
 
 const program = new Command();
 
-program.name('v3mt').version('0.0.1').description('Victoria 3 modding CLI tools');
+program
+  .name('v3mt')
+  .version(packageJson?.version ?? '0.0.0')
+  .description('Victoria 3 modding CLI tools');
 
 init.use(program);
 launchGame.use(program);
